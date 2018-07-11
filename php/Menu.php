ea00bf4ee2bedf66ce7c8dbbe5e29f8e517d2783
sanhueza <tomisanhues2@gmail.com>
2018-07-11 11:33:39 -0400

    Structure

---
+++ php/Menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MULTI-GAME</title>
+</head>
+<body>
+</body>
+</html>
+<?php
+
+/* https://www.w3schools.com/howto/howto_css_login_form.asp
+https://www.tutorialspoint.com/php/php_login_example.htm
+https://www.tutorialspoint.com/php/php_mysql_login.htm
+http://dasunhegoda.com/access-java-function-php-phpjava-bridge/560/
+https://www.w3schools.com/php/php_mysql_create.asp
+https://www.w3schools.com/php/php_mysql_insert.asp
+*/
